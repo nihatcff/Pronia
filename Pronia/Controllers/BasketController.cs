81445f9b10059cf3e340b5c0d3b5ee9dce4513df
Nihat Kerimi <nihatrkh-mpa101@code.edu.az>
2026-01-12 00:22:36 +0400

    Basket Finished

--- Pronia/Controllers/BasketController.cs
+++ Pronia/Controllers/BasketController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Pronia.Abstraction;
+using Pronia.Contexts;
+using Pronia.Services;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -42,7 +44,41 @@ namespace Pronia.Controllers
             _context.BasketItems.Update(basketItem);
             await _context.SaveChangesAsync();
 
-            return RedirectToAction("Index");
+            var basketItems = await _service.GetBasketItemsAsync();
+
+            return PartialView("_BasketPartialView", basketItems);
+        }
+
+        public async Task<IActionResult> IncreaseBasketItemCount(int productId)
+        {
+
+            var isExistProduct = await _context.Products.AnyAsync(x => x.Id == productId);
+
+            if (isExistProduct == false)
+                return NotFound();
+
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier) ?? "";
+
+            var isExistUser = await _context.Users.AnyAsync(x => x.Id == userId);
+
+            if (isExistUser == false)
+                return BadRequest();
+
+            var basketItem = await _context.BasketItems.FirstOrDefaultAsync(x => x.AppUserId == userId && x.ProductId == productId);
+
+            if (basketItem == null)
+                return NotFound();
+
+            basketItem.Count++;
+
+
+            _context.Update(basketItem);
+            await _context.SaveChangesAsync();
+
+            var basketItems = await _service.GetBasketItemsAsync();
+
+            return PartialView("_BasketPartialView", basketItems);
+
         }
     }
 }
